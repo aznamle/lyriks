358b08cafed36373d2d119901471d8e171c5557e
aznamle <namle1998@hotmail.com>
2022-10-28 00:56:10 -0700

    lyrics

--- src/components/DetailsHeader.jsx
+++ src/components/DetailsHeader.jsx
@@ -1,5 +1,9 @@
-const DetailsHeader = () => (
-  <div>DetailsHeader</div>
-);
+import { Link } from 'react-router-dom';
+
+const DetailsHeader = ({ artistId, artistData, songData }) => { 
+  return (
+    <div>DetailsHeader</div>
+  )
+};
 
 export default DetailsHeader;
