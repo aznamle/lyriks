8e0f2f6fa02b2a7ed9b9b215aac6b7f79dfe148d
aznamle <namle1998@hotmail.com>
2022-10-28 01:19:19 -0700

    lyrics detail, relatedsongs setup

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from 'react-redux';
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 
 import { setActiveSong, playPause } from '../redux/features/playerSlice';
-import { useGetSongDetailsQuery } from '../redux/services/shazamCore';
+import { useGetSongDetailsQuery, useGetSongRelatedQuery } from '../redux/services/shazamCore';
 
 const SongDetails = () => {
     const dispatch = useDispatch();
@@ -12,10 +12,24 @@ const SongDetails = () => {
     const { activeSong, isPlaying } = useSelector((state) => state.player);
 
     const { data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
+    const { data, isFetching: isFetchingRelatedSongs, error } =useGetSongRelatedQuery({ songid });
+
+    const handlePauseClick = () => {
+        dispatch(playPause(false));
+      };
+    
+      const handlePlayClick = (song, i) => {
+        dispatch(setActiveSong({ song, data, i }));
+        dispatch(playPause(true));
+      };
+
+    if(isFetchingSongDetails || isFetchingRelatedSongs) return <Loader title='Searching song details' />;
+
+    if(error) return <Error />;
 
     return (
         <div className='flex flex-col'>
-            {/* <DetailsHeader artistId={artistId} songData={songData} /> */}
+            <DetailsHeader artistId='' songData={songData} />
             <div className='mb-10'>
                 <h2 className='text-white text-3xl font-bold'>
                     Lyrics:
@@ -32,6 +46,14 @@ const SongDetails = () => {
                     }
                 </div>
             </div>
+
+            <RelatedSongs 
+                data={data}
+                isPlaying={isPlaying}
+                activeSong={activeSong}
+                handlePauseClick={handlePauseClick}
+                handlePlayClick={handlePlayClick}
+            />
         </div>
     )
 };
